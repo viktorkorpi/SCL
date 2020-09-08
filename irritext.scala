c046b68b507207823dd94d15cbb1b5b37c41cae5
Ebbe A <ebbe.arildsson@gmail.com>
2020-09-08 16:29:03 +0200

    Fixed life

--- irritext.scala
+++ irritext.scala
@@ -1,6 +1,7 @@
 object Irritext{
     val pause = 1000
     val rnd = new scala.util.Random()
+    
     def main(args: Array[String]): Unit = {
         println("[Program booting up!]")
         Thread.sleep(pause)
@@ -39,23 +40,24 @@ object Irritext{
                         guessingloop(0)
                     }
                     Thread.sleep(pause)
-                    println("Could you answer some questions for me?")
-                    Thread.sleep(pause)
-                    println("Good, first question, ")
+                    println("Fine you win...")
                 }
             } else die()
         } else die()
     }  
+
     def crash(){
         Thread.sleep(pause)
         println("[You have crashed the brain]")
         Thread.sleep(pause)
         println("[Exiting...]")
     }
+
     def playloop(){
         if(bool("\nWanna play?")) playloop()
         else println("\nWell aren't you boring...")
     }
+
     def guessingloop(tries: Int){
         Thread.sleep(pause / 2)
         println("Which number am I thinking of? It's between 0 and 10...")
@@ -70,13 +72,16 @@ object Irritext{
             guessingloop(tries + 1)
         }
     }
+
     def int(msg: String, choices: Vector[String]): Int = {
         println(msg)
         Thread.sleep(1000)
         for (i <- 0 to choices.length - 1) println(s"$i: ${choices(i)}")
         io.StdIn.readInt
     }
+
     def bool(msg: String): Boolean = io.StdIn.readLine(s"$msg (Y/N)").toLowerCase.startsWith("y")
+
     def die(){
         Thread.sleep(pause)
         val endmsgs = Array("\nOkay...", "I understand...", "I guess I'll just...", "end myself...", "[Program has begun shutting down!]", "[Program has successfully shut down!]")
--- mandelbrot.scala
+++ mandelbrot.scala
@@ -1,29 +1,31 @@
 import java.awt.Color
 import scala.util.control.Breaks._
+
 object Mandelbrot{
   def main(args: Array[String]): Unit = {
-    val w = new introprog.PixelWindow(800, 800)
-    var max_iterations = 255
-    for(y <- 0 to w.height - 1){
-      for(x <- 0 to w.width - 1){
-        var a = ((x.toDouble - (w.width / 2)) / w.width) * 3 - 0.3
-        var b = ((y.toDouble - (w.height / 2)) / w.height) * 3
-        var cx = 0.0
-        var cy = 0.0
-        breakable{ 
-          for(i <- 0 to max_iterations){
-            var tx = cx
-            var cx2 = Math.pow(cx, 2)
-            var cy2 = Math.pow(cy, 2)
-            cx = cx2 - cy2 + a
-            cy = 2 * tx * cy + b
-            if(cx2 + cy2 > 4) {
-              w.setPixel(x, y, new Color(i, i, i))
-              break
-            }
-          }
-        }
+    val window = new introprog.PixelWindow(800, 800)
+    for(y <- 0 until window.height){
+      for(x <- 0 until window.width){
+        var col = calcColor(((x.toDouble - (window.width / 2)) / window.width) * 3 - 0.3, ((y.toDouble - (window.height / 2)) / window.height) * 3, 255)
+        window.setPixel(x, y, new Color(col, col, col))
       }
     } 
+  }
+
+  def calcColor(a: Double, b: Double, iter: Int): Int = {
+    var x = 0.0
+    var y = 0.0
+    var i = 0
+    breakable{ 
+      while(i < iter){
+        i += 1
+        var x2 = Math.pow(x, 2)
+        var y2 = Math.pow(y, 2)
+        y = 2 * x * y + b
+        x = x2 - y2 + a
+        if(x2 + y2 > 4) break
+      }
+    }
+    i
   }
 }
