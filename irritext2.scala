3d02d72b711d119cabd9719422986d5af19d1190
Ebbe A <ebbe.arildsson@gmail.com>
2020-09-18 01:15:36 +0200

    Updated irritext2

--- irritext2.scala
+++ irritext2.scala
@@ -1,47 +1,45 @@
 object irritext2{
     var rnd = new scala.util.Random()
+    var p = 0
     def main(args: Array[String]): Unit = {
-        for(i <- 0 to 100) {
-            print(s"\r[Loading][${"■" * i + "-" * (100 - i)}][$i%]")
-            Thread.sleep(100)
-        }
-        say("")
-        say("*Welcome Recruit, please report to the bridge immediately!*")
-        say("You are in a filthy grey room")
-        say("You are laying in a gray metall bed")
-        say("It's cold and quiet")
-        if(booleanQuestion("Do you want to get up?", "y/n")){
-            say("You get up and look around")
-            say("There is another bed, some random items and a large metall door")
-            say("You walk over to the door")
-            say("There is a code lock on the door, you will have to guess the code")
-            codeLock()
-            say("The door opens, it was never locked...")
-            say("Outside the door you find a long dark hallway")
-            say("Down the left of the hallway there is a turn and you can see some light, to the right there is only darkness...")
-            if(booleanQuestion("Which way do you want to go?", "l/r")){
-                
-            }
-        } else say("You fall back to sleep")
+        print("\u001b[2J")
+        say("Welcome to PROGRESSION\n(Press ENTER to continue)")
+        say("Your goal is to fill the progressbar to 100%")
+        progressBar(p, p)
+        say("As you can see, it is currently at 0%")
+        say("So, how do you fill it you ask?")
+        say("Well it is quite simple, here I will give you a hand with the first 10%")
+        progressBar(p, p + 10)
+        say("See?")
+        Thread.sleep(100)
+        say("What?")
+        say("You do not understand how to fill the bar?")
+        say("Come on, just fill it already")
+        while(p < 100) fill()
     }
 
-    def codeLock(): Unit = {
-        val input = io.StdIn.readLine.toLowerCase()
-        if(input.startsWith("o")) say("You try to open the door")
-        else if(input.toIntOption.getOrElse(0) == rnd.nextInt(10000)) {
-            say("The lamp on the lock turned green but the there is no sound from the door")
-            say("You try to open the door")
-        }
-        else {
-            say("The locks lamp blinks red, you will have to guess again")
-            codeLock()
-        }
+    def fill(): Unit = {
+        val in = io.StdIn.readLine()
+        if(in.startsWith("+")) progressBar(p, p + 1)
+        else if(in.startsWith("*")) progressBar(p, p * 2)
     }
 
-    def booleanQuestion(msg: String, options: String): Boolean = {
+    def boolQ(msg: String, options: String): Boolean = { 
         say(s"$msg ($options)")
-        io.StdIn.readLine().toLowerCase.startsWith("y")
+        io.StdIn.readLine().toLowerCase.startsWith(s"${options(0)}")
     }
 
-    def say(msg: Any): Unit = { Thread.sleep(1500); println(msg) }
+    def say(msg: Any): Unit = { 
+        print(msg) 
+        while(io.StdIn.readLine() != ""){}
+    }
+
+    def progressBar(previous_progress: Int, progress: Int): Unit = {
+        for(i <- previous_progress to progress) {
+            print(s"\r[Progress][${"■" * i}${"-" * (100 - i)}][$i%]")
+            Thread.sleep(75)
+        }
+        println("")
+        p = progress
+    }
 }
